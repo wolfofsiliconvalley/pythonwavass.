b3bc40c1f8582acad05c6c31e817ba73e7dcf6a0
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-07-30 01:32:51 +0100

    Update Lab1of2.py

--- Labs/Lab1of2.py
+++ Labs/Lab1of2.py
@@ -10,8 +10,4 @@ print('\nThe number of tiles left over')
 print(17*6-(9*7+5*7))
 
 
-# Fill this in with an expression that calculates how many tiles are needed.
-print()
 
-# Fill this in with an expression that calculates how many tiles will be left over.
-print()
