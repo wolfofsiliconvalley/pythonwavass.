0914d684934c97da6cd69c5027c90fc11c5fd392
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 02:06:08 +0100

    Update Lab1of6.py

--- Labs/Lab1of6.py
+++ Labs/Lab1of6.py
@@ -1,13 +1,10 @@
-username = "Kinari"
-timestamp = "04:50"
-url = "http://petshop.com/pets/mammals/cats"
 
-# TODO: print a log message using the variables above.
-# The message should have the same format as this one:
-# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
 
+
+# storing my variables
  username="Kinari accessed the site"
  url="http://petshop.com/pets/mammals/cats at "
  timestamp="04:50"
+ # printing in the format "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
  print(username+" "+url+""+timestamp)
  
