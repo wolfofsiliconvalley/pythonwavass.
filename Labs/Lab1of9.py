b29bc02dd9522fa7846ed61855c80a709861e779
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 02:30:07 +0100

    Update Lab1of9.py

--- Labs/Lab1of9.py
+++ Labs/Lab1of9.py
@@ -3,21 +3,36 @@
 #and try them out here. Endeavor to copy the link without the first # comment sign beginning it.
 
 python = "franklin cole"
+# string method, capitalize
 print(python.capitalize())
+# string method, casefold
 print(python.casefold())
+# string methos, center
 print(python.center(20))
+# string method, count
 print(python.count("cole"))
+# string method, encode
 print(python.encode(encoding="utf-8",errors="strict"))
+# string method, endswith
 print(python.endswith("cole"))
+# string method, endswith
 print(python.endswith("franklin"))
+# changing variable and string
 python = "f\ra\nk\li\n c\ol\e"
+# string method, expandtabs
 print(python.expandtabs(8))
-python="franklin cole"
+# changing variable and string back to python = "franklin cole"
+python = "franklin cole"
+# string method, find
 print(python.find("klin"))
+# string method, format
 print(python.format())
+# string method, format
 python = "My name is {name}".format(name="Franklin")
 print(python)
+# string method, islower
 print(python.islower())
+# string method, lower
 print(python.lower())
 
 
