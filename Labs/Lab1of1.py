a5009bb6b1ec0e70e56641b0690af9bc13daa151
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 01:36:29 +0100

    Update Lab1of1.py

--- Labs/Lab1of1.py
+++ Labs/Lab1of1.py
@@ -1,10 +1,2 @@
-#Quiz: Average Electricity Bill
-#It's time to try a calculation in Python!
-
-
-
-# Write an expression that calculates the average of 23, 32 and 64
-# Place the expression in this print statement
-
 #computes the average of 23, 32 and 64
 print((23+32+64)/3)
