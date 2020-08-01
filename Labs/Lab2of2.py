abef38de1a0f13e731c6fcc83cece0a19d3f41b4
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 02:36:53 +0100

    Update Lab2of2.py

--- Labs/Lab2of2.py
+++ Labs/Lab2of2.py
@@ -1,18 +1,7 @@
-#1. What is the length of the string variable verse?
-#2. What is the index of the first occurrence of the word 'and' in verse?
-#3. What is the index of the last occurrence of the word 'you' in verse?
-#4. What is the count of occurrences of the word 'you' in the verse?
-
-
 verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
 print(verse)
 
-# Use the appropriate functions and methods to answer the questions above
-1)len(verse)
-2)elm = "and"
-  print(verse.index(elm))
-3)elm = "you"
-  print(len(verse) - verse[::-1].index(elm) - 1)
-# Bonus: practice using .format() to output your answers in descriptive messages!
-verse = "If you can keep your head when all about you\n  Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n  But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n  Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n  And yet don’t look too good, nor talk too wise:"
-print(verse.format())
+print('The length of the string is: {}'.format(len(verse)))
+print("\nThe index of the first occurences of the word 'and' is: {}".format(verse.find('and')))
+print("\nThe index of the last occurences of the word 'you' is: {}".format(verse.rfind("you")))
+print("\nThe count of occurences of the word 'you' in the verse is: {}".format(verse.count("you")))
