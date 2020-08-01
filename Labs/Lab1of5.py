08aacdde205bf24fd34a305d94629d100038d29c
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 02:03:31 +0100

    Update Lab1of5.py

--- Labs/Lab1of5.py
+++ Labs/Lab1of5.py
@@ -1,8 +1,5 @@
-#The line of code in the following quiz will cause a SyntaxError, thanks to the misuse of quotation marks. First run it with  to view the error message. Then resolve the problem so that the quote (from Henry Ford) is correctly assigned to the variable ford_quote.
 
 
-# TODO: Fix this string!
+# fixing this line of code -ford_quote = 'Whether you think you can, or you think you can't--you're right.'
 
-ford_quote = 'Whether you think you can, or you think you can't--you're right.'
-
-ans. ford_quote = 'Whether you think you can, or you think you can\'t--you\'re right.'
+ford_quote = 'Whether you think you can, or you think you can\'t--you\'re right.'
