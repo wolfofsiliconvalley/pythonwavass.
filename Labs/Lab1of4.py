6a72be13e057e93f4ea17f0d8df5095614eefd2c
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 02:01:25 +0100

    Update Lab1of4.py

--- Labs/Lab1of4.py
+++ Labs/Lab1of4.py
@@ -8,8 +8,10 @@ rio_de_janeiro_pop_density = rio_population/rio_area
 
 sf_population, sf_area = 864816, 231.89
 rio_population, rio_area = 6453682, 486.5
+# calculating the population density for san francisco
 san_francisco_pop_density = sf_population/sf_area
 print(san_francisco_pop_density)
+# calculating the population density for rio de janeiro
 rio_de_janeiro_pop_density = rio_population/rio_area
 print(rio_de_janeiro_pop_density)
 if san_francisco_pop_density > rio_de_janeiro_pop_density:
