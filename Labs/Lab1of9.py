23b02b0866ee3efdfc1d25e53dc23001a46ad82e
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 02:31:06 +0100

    Update Lab1of9.py

--- Labs/Lab1of9.py
+++ Labs/Lab1of9.py
@@ -1,6 +1,3 @@
-# Browse the complete list of string methods at:
-# https://docs.python.org/3/library/stdtypes.html#string-methods 
-#and try them out here. Endeavor to copy the link without the first # comment sign beginning it.
 
 python = "franklin cole"
 # string method, capitalize
