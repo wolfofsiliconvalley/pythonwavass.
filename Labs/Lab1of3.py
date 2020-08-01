8f839a0179be52def07d3c486590955386677cfc
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 01:58:07 +0100

    Update Lab1of3.py

--- Labs/Lab1of3.py
+++ Labs/Lab1of3.py
@@ -1,24 +1,24 @@
-#Note that this code uses scientific notation to define large numbers. 4.445e8 is equal to 4.445 * 10 ** 8 which is equal to 444500000.0.
+
 
 # The current volume of a water reservoir (in cubic metres)
 reservoir_volume = 4.445e8
 # The amount of rainfall from a storm (in cubic metres)
 rainfall = 5e6
 
-# decrease the rainfall variable by 10% to account for runoff
+# decreasing the rainfall variable by 10% to account for runoff
 
 rainfall = 5*10**6
 runoff = rainfall * 0.1
 rainfall = rainfall - runoff
 
 
-# add the rainfall variable to the reservoir_volume variable
+# adding the rainfall variable to the reservoir_volume variable
 
 reservoir_volume = 4.445*10**8
 reservoir_volume = reservoir_volume + rainfall
 
 
-# increase reservoir_volume by 5% to account for stormwater that flows into the reservoir in the days following the storm
+# increasing reservoir_volume by 5% to account for stormwater that flows into the reservoir in the days following the storm
 
 stormwater = reservoir_volume * 0.5
 reservoir_volume = reservoir_volume + stormwater
@@ -27,7 +27,7 @@ reservoir_volume = reservoir_volume + stormwater
 
 
 
-# decrease reservoir_volume by 5% to account for evaporation
+# decreasing reservoir_volume by 5% to account for evaporation
 
 evaporation = reservior_volume * 0.5
 reservoir_volume = reservoir_volume - evaporation
@@ -39,7 +39,7 @@ reservoir_volume = reservoir_volume - 250000.0
 
 
 
-# print the new value of the reservoir_volume variable
+
 print(reservoir_volume)
 
-ans.336500000.0
+
