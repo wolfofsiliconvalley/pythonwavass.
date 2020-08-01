5a81e1aad810f06bda2073a6fd53541b5bf18129
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 01:56:05 +0100

    Update Lab1of2.py

--- Labs/Lab1of2.py
+++ Labs/Lab1of2.py
@@ -4,7 +4,7 @@ print('The number of tiles needed')
 print((9*7) + (5*7))
 
 # after buying 17 packages of tiles containing 6 tiles each. How many tiles will be left over?
-print('\nThe number of tiles left over')
+print('\n The number of tiles left over')
 print(17*6-(9*7+5*7))
 
 
