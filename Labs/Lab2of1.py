66a0189cf41c163fa61b976787bc3571bb3c3e85
wolfofsiliconvalley <61973246+wolfofsiliconvalley@users.noreply.github.com>
2020-08-01 02:33:31 +0100

    Update Lab2of1.py

--- Labs/Lab2of1.py
+++ Labs/Lab2of1.py
@@ -1,10 +1,7 @@
-# Write two lines of code below, each assigning a value to a variable
+# assigning values to variables
 fname = Franklin
 age = 20
 
-# Now write a print statement using .format() to print out a sentence and the 
-#   values of both of the variables
-
-
+#  writing a print statement using .format() to print out a sentence 
 txt1 = "My name is {fname}, I'm {age}".format(fname = "Franklin", age = 20)
 print(txt1)
